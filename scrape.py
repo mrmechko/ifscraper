0f26c30db836373c155191473f9ae2a6ab63b22e
Rik Bose <rbose@knox.edu>
2021-07-03 23:16:42 -0500

    grabbing username upvotes, views, etc

--- scrape.py
+++ scrape.py
@@ -11,6 +11,15 @@ parser.add_argument("--url", help="image flip url")
 parser.add_argument("--num", help="number of memes to scrape (default=10)", default=10, type=int)
 args = parser.parse_args()
 
+template = """<div class="base-unit clearfix"><h2 class="base-unit-title"><a href={href}>{title}</a></h2><div class="base-img-wrap-wrap"><div class="base-img-wrap" style="{img_width_wrap}"><a class="base-img-link" href="{base_link}" style="padding-bottom:112.5%"><img class="base-img" src="{img}" alt="{alt}"></a></div></div><div class="base-info"><div class="base-author">by <a class="u-username" href="{usr_name_url}">{usr}</a></div><div class="base-view-count">{info}</div>"""
+# href
+# title
+# img_width_wrap
+# img
+# alt
+# view_count
+# usr
+# upvotes
 
 def get_name(url, output):
     trimmed = re.sub(r'https?:\/\/', '', url)
@@ -41,7 +50,6 @@ def scrape(url, output, num, replace=False):
         os.mkdir(img_dir)
 
     session = HTMLSession()
-    template = "<img class=\"base-img\" src=\"{img}\" alt=\"{alt}\">"
     page = session.get(url)
     res = []
 
@@ -51,7 +59,9 @@ def scrape(url, output, num, replace=False):
             image_url = "https:"+r["img"]
             image_name = get_name(image_url, output)
             get_img(image_url, image_name, replace=replace)
-            res.append(dict(img=image_name, url=image_url, alt=r["alt"]))
+            meta = dict(img=image_name, url=image_url, alt=r["alt"], info=r["info"], user=r["usr"])
+            res.append(meta)
+            print(json.dumps(meta, indent=2))
             time.sleep(2)
         next_url = page.html.next()
         page = session.get(next_url)
